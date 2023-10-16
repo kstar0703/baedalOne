181540a280ac98a3d26d5c6033ee26127ccfaf5a
rinklove <88640680+rinklove@users.noreply.github.com>
2023-10-17 08:37:34 +0900

    docs: cartVo, cartListVo 추가

--- javaWorkspace/Prj01/src/com/baedal/one/purchase/dao/PurchaseDao.java
+++ javaWorkspace/Prj01/src/com/baedal/one/purchase/dao/PurchaseDao.java
@@ -1,13 +1,17 @@
 package com.baedal.one.purchase.dao;
 
+import java.sql.Connection;
+
+import com.baedal.one.purchase.vo.CartVo;
+
 public class PurchaseDao {
 
 	private String query;
 	
-	public /*List<Menu>*/ printMenu(/*메뉴 번호*/, /*Connection 객체*/) {
+	public /*List<Menu>*/ printMenu(/*메뉴 번호*/, Connection conn) {
 		query = "SELECT FROM MENU M WHERE M.STORE_NO = ? AND M.DELETE_YN = 'N' AND M.SELL_YN = 'Y'";
 		/*
-		 * PreparedStatement pstmt = Connection 객체.prepareStatement(query);
+		 * PreparedStatement pstmt = conn.prepareStatement(query);
 		 * pstmt.setString(1, 메뉴 번호);
 		 * ResultSet rs = pstmt.executeQuery();
 		 * List<Menu> menuList = null;
@@ -18,5 +22,10 @@ public class PurchaseDao {
 		 * 
 		 * return menuList;
 		 */
+	}
+
+
+	public CartVo getMyCart(/*회원번호*/ ,Connection conn) {
+		query = "SELECT A.* FROM CART A LEFT OUTER JOIN ORDERS B ON A.CART_NO = B.CART_NO WHERE A.USER_NO = ? AND B.ORDER_NO IS NULL";
 	}
 }
--- javaWorkspace/Prj01/src/com/baedal/one/purchase/service/PurchaseService.java
+++ javaWorkspace/Prj01/src/com/baedal/one/purchase/service/PurchaseService.java
@@ -1,7 +1,11 @@
 package com.baedal.one.purchase.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import com.baedal.one.purchase.dao.PurchaseDao;
 import com.baedal.one.purchase.vo.CartVo;
+import com.kh.app.jdbc.JDBCTemplate;
 
 public class PurchaseService {
 
@@ -10,15 +14,28 @@ public class PurchaseService {
 	public PurchaseService() {
 		purchaseDao = new PurchaseDao();
 	}
-	public /*Menu*/ printMenu(/*매장 번호*/) {
+	
+	public /*Menu<List>*/ void printMenu(/*매장 번호*/) throws Exception {
 		//Connection
+		Connection conn = JDBCTemplate.getConnection();
+		 /* List<Menu> menuList =*/ purchaseDao.printMenu(/*매장 번호*/, conn);
 		
 		//close
-		purchaseDao.printMenu(/*매장 번호*/, /*Connection 객체*/);
-		
+		 JDBCTemplate.close(conn);
+		//return menuList;
 	}
-	public CartVo getMyCart() {
-		// TODO Auto-generated method stub
+	public CartVo getMyCart(/*회원번호*/) throws Exception {
+		Connection conn = JDBCTemplate.getConnection();
+		
+		purchaseDao.getMyCart(/*회원 번호*/, conn);
+		JDBCTemplate.close(conn);
 		return null;
+	}
+	
+	public int deleteCartList(/*회원번호*/) throws Exception {
+		//Connection
+		Connection conn = JDBCTemplate.getConnection();
+		JDBCTemplate.close(conn);
+		return 0;
 	}
 }
