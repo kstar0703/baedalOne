f43a2aec5299ae65b1753cf1ddf260481c70f051
rinklove <88640680+rinklove@users.noreply.github.com>
2023-10-16 18:31:07 +0900

    chore: 패키지 구조 변경

--- javaWorkspace/Prj01/src/com/baedal/one/Main.java
+++ javaWorkspace/Prj01/src/com/baedal/one/Main.java
@@ -1,4 +1,4 @@
-package main;
+package com.baedal.one;
 
 public class Main {
 	
--- javaWorkspace/Prj01/src/main/kimheesung.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class kimheesung {
-	
-	public static void main(String[] args) {
-		System.out.println("테스트");
-	}
-
-}
