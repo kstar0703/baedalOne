6d082357bd446d93cb79fc724a6b291bfd2fa3ec
kstar0703 <kstar966@gmail.com>
2023-10-18 01:12:11 +0900

    세이브

--- javaWorkspace/Prj01/src/com/baedal/one/owner/vo/OwnerVo.java
+++ javaWorkspace/Prj01/src/com/baedal/one/owner/vo/OwnerVo.java
@@ -3,14 +3,10 @@ package com.baedal.one.owner.vo;
 public class OwnerVo {
 	
 	private String ownerNo;	 
-	private String	wnerId;	  
+	private String	ownerId;	  
 	private String	ownerPwd;	   
 	private String	enrollDate;	
 	private String	quitYn;
-	
-	
-	
-	
 	
 	
 	
@@ -20,15 +16,13 @@ public class OwnerVo {
 
 
 
-
-	public OwnerVo(String ownerNo, String wnerId, String ownerPwd, String enrollDate, String quitYn) {
+	public OwnerVo(String ownerNo, String ownerId, String ownerPwd, String enrollDate, String quitYn) {
 		this.ownerNo = ownerNo;
-		this.wnerId = wnerId;
+		this.ownerId = ownerId;
 		this.ownerPwd = ownerPwd;
 		this.enrollDate = enrollDate;
 		this.quitYn = quitYn;
 	}
-
 
 
 
@@ -40,7 +34,6 @@ public class OwnerVo {
 
 
 
-
 	public void setOwnerNo(String ownerNo) {
 		this.ownerNo = ownerNo;
 	}
@@ -48,19 +41,16 @@ public class OwnerVo {
 
 
 
-
-	public String getWnerId() {
-		return wnerId;
+	public String getOwnerId() {
+		return ownerId;
 	}
 
 
 
 
-
-	public void setWnerId(String wnerId) {
-		this.wnerId = wnerId;
+	public void setOwnerId(String ownerId) {
+		this.ownerId = ownerId;
 	}
-
 
 
 
@@ -72,11 +62,9 @@ public class OwnerVo {
 
 
 
-
 	public void setOwnerPwd(String ownerPwd) {
 		this.ownerPwd = ownerPwd;
 	}
-
 
 
 
@@ -88,11 +76,9 @@ public class OwnerVo {
 
 
 
-
 	public void setEnrollDate(String enrollDate) {
 		this.enrollDate = enrollDate;
 	}
-
 
 
 
@@ -104,7 +90,6 @@ public class OwnerVo {
 
 
 
-
 	public void setQuitYn(String quitYn) {
 		this.quitYn = quitYn;
 	}
@@ -112,12 +97,18 @@ public class OwnerVo {
 
 
 
-
 	@Override
 	public String toString() {
-		return "OwnerVo [ownerNo=" + ownerNo + ", wnerId=" + wnerId + ", ownerPwd=" + ownerPwd + ", enrollDate="
+		return "OwnerVo [ownerNo=" + ownerNo + ", ownerId=" + ownerId + ", ownerPwd=" + ownerPwd + ", enrollDate="
 				+ enrollDate + ", quitYn=" + quitYn + "]";
-	}	   
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 		
