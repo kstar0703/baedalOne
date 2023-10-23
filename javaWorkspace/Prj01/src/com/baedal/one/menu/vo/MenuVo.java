194260bd6709aa70b52bd7cce8a410a5a6b7400c
DESKTOP-52HLHV2\s <katie8454@naver.com>
2023-10-23 11:01:53 +0900

    객체 저장 ksh_1023_01

--- javaWorkspace/Prj01/src/com/baedal/one/menu/vo/MenuVo.java
+++ javaWorkspace/Prj01/src/com/baedal/one/menu/vo/MenuVo.java
@@ -2,12 +2,15 @@ package com.baedal.one.menu.vo;
 
 public class MenuVo {
 	
-	private String menuNo;
-	private String storeNo;
+	private String menuNo;		//메뉴번호
+	private String storeNo;		//매장번호
 	private String menuName;
 	private String price;
-	private String deleteYn;
-	private String sellYn;
+	private String deleteYn;	//삭제여부
+	private String sellYn;		//노출여부
+//	private boolean isFeatured; // 대표 메뉴 여부
+//    private boolean isPopular; 	// 인기 메뉴 여부
+
 	
 	public MenuVo(String menuNo, String storeNo, String menuName, String price, String deleteYn, String sellYn) {
 		super();
@@ -17,6 +20,8 @@ public class MenuVo {
 		this.price = price;
 		this.deleteYn = deleteYn;
 		this.sellYn = sellYn;
+//		this.isFeatured = isFeatured;
+//		this.isPopular = isPopular;
 	}
 	
 	public MenuVo() {
@@ -60,6 +65,7 @@ public class MenuVo {
 	public void setSellYn(String sellYn) {
 		this.sellYn = sellYn;
 	}
+	
 	
 	@Override
 	public String toString() {
